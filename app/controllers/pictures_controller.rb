8fe99d86818839cf3754c92fe4925ec352a33e84
Gina Son <ginasunah33@gmail.com>
2017-07-18 15:18:14 -0400

    completed create

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,28 @@
 class PicturesController < ApplicationController
   def index
-
+    @pictures = Picture.all
   end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    @picture = Picture.new
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
+  end
+
 end
